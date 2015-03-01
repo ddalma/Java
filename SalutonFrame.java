842aab414701d583e53c6f04b924bb7c4681d786
ddalma01 <student@172-16-12-155.dynapool.nyu.edu>
2015-03-01 16:12:04 -0500

    First Java GUI
    First use of Java, making a GUI!

---
+++ SalutonFrame.java
@@ -0,0 +1,23 @@
+ import javax.swing.*;
+ 
+ public class SalutonFrame extends JFrame {
+    public SalutonFrame() {
+        super("Saluton mondo!");
+        setLookAndFeel();
+        setSize(350, 100);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+ 
+    private void setLookAndFeel() {
+        try {
+            UIManager.setLookAndFeel( "com.java.swing.plaf.nimbus.NimbusLookAndFeel");
+        } catch (Exception exc) {
+            // ignore error
+        }
+    }
+ 
+    public static void main(String[] arguments) {
+        SalutonFrame sal = new SalutonFrame();
+    }
+ }
